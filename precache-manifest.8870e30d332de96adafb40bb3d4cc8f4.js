78ff6715c624fb3ff14c721240e61de92037b60d
son <hoangsonFDS@gmail.com>
2022-12-14 18:18:40 +0700

    Deploy React app to GitHub Pages

---
+++ precache-manifest.8870e30d332de96adafb40bb3d4cc8f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0943286fd17829109e4c",
+    "url": "/react-gh-pages/static/css/main.3d887eb0.chunk.css"
+  },
+  {
+    "revision": "0943286fd17829109e4c",
+    "url": "/react-gh-pages/static/js/main.0943286f.chunk.js"
+  },
+  {
+    "revision": "289efe08ed6fb1b3416a",
+    "url": "/react-gh-pages/static/js/1.289efe08.chunk.js"
+  },
+  {
+    "revision": "dfad6922584d01950ab9",
+    "url": "/react-gh-pages/static/js/2.dfad6922.chunk.js"
+  },
+  {
+    "revision": "a1ca891825d131d2d04b",
+    "url": "/react-gh-pages/static/js/runtime~main.a1ca8918.js"
+  },
+  {
+    "revision": "0bc8a6563ed2c33a2db9072a8d9fa168",
+    "url": "/react-gh-pages/static/media/download.0bc8a656.svg"
+  },
+  {
+    "revision": "65c1d34dce953843a14ba55c1ddd7e61",
+    "url": "/react-gh-pages/index.html"
+  }
+];
